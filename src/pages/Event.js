769d9de901427a15d302e0bc010f46d57fab9351
Chris Tri <christophertri90@gmail.com>
2020-01-19 10:42:58 +1100

    added additional routes

---
+++ src/pages/Event.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Event extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Event</h1>
+      </div>
+    )
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import Events from "../pages/Events";
+import Event from "../pages/Event";
+import SignUp from "../pages/SignUp";
 
 const Router = () => {
   return (
@@ -10,6 +12,7 @@ const Router = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/events" component={Events} />
+          <Route exact path="/signup" component={SignUp} />
         </Switch>
       </BrowserRouter>
     </div>
