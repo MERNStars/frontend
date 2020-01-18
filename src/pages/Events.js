1dfe01acacdf449d221273d56ccd5e8e17d73903
Chris Tri <christophertri90@gmail.com>
2020-01-19 10:31:04 +1100

    added routes

---
+++ src/pages/Events.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Events extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Events</h1>
+      </div>
+    )
+  }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
