b3b5011addfa906437ea6b3f3389d6bc4691d16a
Chris Tri <christophertri90@gmail.com>
2020-01-20 03:04:58 +1100

    added new form

--- src/components/attendform.js
+++ src/components/attendform.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-
-
-
+import Axios from "axios";
 
 class AttendForm extends Component {
-  
-  
-  
+  state = {
+    event: null
+  };
+  // TODO Retrieve event ID from event page
+  // TODO Retrieve user id then assign to attendee Schema
+  // TODO Create redux form for user to enter friends detail and dependents
+  // TODO Store data into event's attribute through the attendees Schema
+
   render() {
-    return <div>Hello World</div>;
+    // const { _id } = this.state.event;
+    return <div></div>;
   }
 }
+
+export default AttendForm;
