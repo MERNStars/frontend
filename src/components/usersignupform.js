a30d133abb6d327bc073e3731965445c2d09df43
Chris Tri <christophertri90@gmail.com>
2020-01-19 12:11:33 +1100

    added usersign up form and changed signup page

---
+++ src/components/usersignupform.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserSignUpForm = () => {
+  return <h1>User Sign Up Form</h1>;
+};
+
+export default UserSignUpForm;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,3 +9,5 @@ export default class SignUp extends Component {
     )
   }
 }
+
+
