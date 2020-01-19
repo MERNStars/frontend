4db56f923d8b54883960672f4f123d8619ea4fc1
Chris Tri <christophertri90@gmail.com>
2020-01-19 23:27:19 +1100

    added contact form component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ContactForm from "../components/contactform";
 
 export default class Contact extends Component {
   render() {
     return (
       <div>
-        <h1>Contact Us</h1>
+        <ContactForm />
       </div>
-    )
+    );
   }
 }
