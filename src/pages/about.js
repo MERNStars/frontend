ad4facaed298fda447e47dc17ab8d93e25ec89b0
Chris Tri <christophertri90@gmail.com>
2020-01-19 11:57:10 +1100

    added additional routes and created the navbar component to be rendered on all pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+      </div>
+    )
+  }
+}
