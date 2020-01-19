de28ff9e9a613b923877afc0463bb31368d6ca2e
Chris Tri <christophertri90@gmail.com>
2020-01-20 00:15:04 +1100

    added working contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import ContactForm from "../components/contactform";
+import axios from "axios";
+require('dotenv').config()
 
 export default class Contact extends Component {
+  submit = data => {
+    console.log(data);
+    axios
+      .post(`${process.env.BACKEND_DB_URL}/email`, data)
+      .then(response => {
+        console.log(response.data);
+      });
+  };
+
   render() {
     return (
       <div>
-        <ContactForm />
+        <ContactForm onSubmit={this.submit} />
       </div>
     );
   }
