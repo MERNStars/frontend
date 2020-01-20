7387a84b3eb0397be61a2fb9d07ed618a5b05ed6
Izzy Peskett <isabellepeskett@gmail.com>
2020-01-20 15:59:11 +1100

    added semantic UI

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import styles from '../styles/event.module.scss';
 import Moment from 'react-moment';
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 class Event extends React.Component {
 
+  CardExampleCard = () => (
+    <Card>
+      <Image src={ require('../assets/placeholder.jpg')} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{this.props.event_name}</Card.Header>
+        <Card.Meta>
+          <span className='date'><Moment format="D MMM">{this.props.event_date.begin}</Moment></span>
+        </Card.Meta>
+        <Card.Meta><span className='time'>Start Time <Moment format="HH:MM">{this.props.event_date.begin}</Moment></span></Card.Meta>
+        <Card.Description>
+          {this.props.event_category}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          {this.props.event_capacity}
+        </a>
+      </Card.Content>
+    </Card>
+  )
 
   render() {
     console.log( this.props)
-    const { event_name, event_date } = this.props
     return(
-      <div className = {styles.eventCard}>
-        <div className = {styles.top}>
-          <div className = {styles.eventDate}><sub><Moment format="D MMM">{event_date.begin}</Moment></sub></div>
-          <img src={ require('../assets/placeholder.jpg') } alt="fresh fruit on table"></img>
-        </div>
-        <div className = {styles.bottom}>
-          <h1> {event_name}</h1>
-        </div>
+      <div className={styles.eventCard}>
+        <a href="#">{this.CardExampleCard()}</a>
       </div>
     )
   }
