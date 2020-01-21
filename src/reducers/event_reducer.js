4cc6bbb8656227e62e7e045810b21a4c91fa4929
Chris Tri <christophertri90@gmail.com>
2020-01-21 13:59:32 +1100

    added form and added picker for presenter

--- src/reducers/event_reducer.js
+++ src/reducers/event_reducer.js
@@ -1,28 +1,85 @@
-const initialState = { 
-    
-    event_categories: ["bible seminar", "career seminar", "exercise class", "health seminar", "healthy cooking", "lifestyle change workshop", "mental health workshop", "massage service", "others", "addiction recovery", "weight-loss program"],
-    
-    event_statuses: ["scheduled", "canceled", "postponed", "completed", "sold out"],
+import Axios from "axios";
+require("dotenv").config();
 
-    message: ''
-}
+const initialState = {
+  event_categories: [
+    "bible seminar",
+    "career seminar",
+    "exercise class",
+    "health seminar",
+    "healthy cooking",
+    "lifestyle change workshop",
+    "mental health workshop",
+    "massage service",
+    "others",
+    "addiction recovery",
+    "weight-loss program"
+  ],
+  event_statuses: [
+    "scheduled",
+    "canceled",
+    "postponed",
+    "completed",
+    "sold out"
+  ],
 
+  message: ""
+};
 
-const eventReducer = (state=initialState, action) => {
+export const createEvent = event => {
+  return dispatch => {
+    console.log(`${event.event_date}T${event.event_start_time}`);
+    console.log(`${event.event_date}T${event.event_end_time}`);
+    Axios.post(
+      `http://localhost:8888/events/create`,
+      {
+        event_name: event.event_name,
+        description: event.description,
+        event_date: {
+          begin: `${event.event_date}T${event.event_start_time}`,
+          end: `${event.event_date}T${event.event_end_time}`
+        },
+        registration_closed_date: `${event.registration_closed_date}T23:55:55.000Z`,
+        presenters: ["5e2001deccc10275c4a1a11f"],
+        is_family_friendly: event.is_family_friendly,
+        minimum_age: event.minimum_age,
+        event_category: event.event_category,
+        status: event.status,
+        images: event.images,
+        event_capacity: event.event_capacity,
+        published: event.published
+      },
+      {
+        headers: {
+          authorization: `${localStorage.weexplore_token}`
+        }
+      }
+    ).then(response => {
+      console.log(response);
+      dispatch(eventCreated(response.data));
+    });
+  };
+};
 
-    let newState = {};
-    switch(action.type){
-        case "EVENT_CREATED":
-            newState = { ...state, message: "Your event has been created." };
-            break;
-        case "EVENT_DELETED":
-            newState = { ...state, message: "Your event has been deleted." };
-            break;    
-        default:
-            newState = { ...state};
-            break;
-    }
-    return newState;
-}
+const eventCreated = message => ({
+  type: "EVENT_CREATED",
+  message: message
+});
+
+const eventReducer = (state = initialState, action) => {
+  let newState = {};
+  switch (action.type) {
+    case "EVENT_CREATED":
+      newState = { ...state, message: "Your event has been created." };
+      break;
+    case "EVENT_DELETED":
+      newState = { ...state, message: "Your event has been deleted." };
+      break;
+    default:
+      newState = { ...state };
+      break;
+  }
+  return newState;
+};
 
 export default eventReducer;
