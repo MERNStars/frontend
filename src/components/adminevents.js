195c82d065e5cfe63b9222b07cbb5cac2108c10f
Izzy Peskett <isabellepeskett@gmail.com>
2020-01-22 20:46:36 +1100

    Updated admin dashboard event cards

--- src/components/adminevents.js
+++ src/components/adminevents.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
-import EventCard from "../components/eventCard";
+import AdminEventCard from "../components/adminEventCards";
 import styles from "../styles/admin.module.scss";
 
+
 export default class AdminEvents extends Component {
+
+  createEvent() {
+    // Insert code to create a new event. 
+  }
+
+  editEvent = () => {
+    // Insert code to grab event with axios call and patch event. 
+  }
+
+  deleteEvent = () => {
+    // Insert code to grab event with axios call and delete event. 
+  }
+
+  publishEvent = () => {
+    // Insert code to grab event with axios and change event status from unpublished to publish
+  }
+
   render() {
     return (
       <>
         <h1>Events</h1>
+        <h3>Published Events</h3>
         <div className={styles.eventsContainer}>
           {this.props.events
             ? this.props.events.map((event, i) => {
-                return <EventCard {...event} index={i} />;
+                return <AdminEventCard {...event} index={i} />;
               })
             : null}
         </div>
