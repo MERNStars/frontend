f4eaadfde53818627c7864f6b9763545ce4e4d3e
Izzy Peskett <isabellepeskett@gmail.com>
2020-01-24 16:31:53 +1100

    Initalised event status control flow

--- src/components/adminevents.js
+++ src/components/adminevents.js
@@ -9,21 +9,33 @@ require('dotenv').config()
 
 export default class AdminEvents extends Component {
 
+  state = {
+    publishedEvents: null,
+    unpublishedEvents: [],
+    pastEvents: []
+  }
+
+  componentDidMount() {
+    let publishArray = this.props.events.filter( event => event.published === true )
+    this.setState( { publishedEvents: publishArray})
+    console.log( this.state.publishedEvents)
+  }
+
   editEvent = event => {
     return(
       <EditEvent {...event}/>
       )
   }
     
-
   render() {
+    console.log( this.state.publishedEvents)
     return (
       <>
       <h3>Published Events</h3>
         <div className={styles.eventsContainer}>
-          {this.props.events
-            ? this.props.events.map((event, i) => {
-                return ( event.published ? <AdminEventCard {...event} index={i} /> : null )})
+          {this.state.publishedEvents
+            ? this.state.publishedEvents.map((event, i) => {
+                return (  <AdminEventCard {...event} index={i} /> )})
             : null}
         </div>
         <h3>Unpublished Events</h3>
