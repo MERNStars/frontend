359fa0f718ee14da92103f11cd915388edb20ad7
Chris Tri <christophertri90@gmail.com>
2020-02-03 13:06:56 +1100

    added enzyme testing library

--- src/test/Events.test.js
+++ src/test/Events.test.js
@@ -1,17 +1,15 @@
 import { CreateEvent } from "../pages/CreateEvent";
+import CreateEventForm from "../components/CreateEventForm";
+import WizardFormFirstPage from '../components/CreateEventForm/createformpageone'
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
 import React from "react";
 import store from "../store";
+import { shallow, mount } from "enzyme";
 
 function wrapper(component) {
   return <Provider store={store}>{component}</Provider>;
 }
-
-describe("Tests Create Event Form Component", () => {
-  const {container} = render(wrapper(<CreateEvent />))
-  console.log(container)
-})
 
 describe("Create Event Component", () => {
   it("tests if create event component renders", () => {
@@ -19,10 +17,12 @@ describe("Create Event Component", () => {
       wrapper(<CreateEvent />)
     );
 
+    console.log(render(
+      wrapper(<CreateEvent />)
+    ).debug())
+
     // expect(queryByText(/Event Name/)).toBeInTheDocument();
     // fireEvent.click(queryByText(`type="submit"`));
-    
-
 
   });
 
