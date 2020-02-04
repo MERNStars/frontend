1af600a9ffd9fba6814b723291ecd57b2a5c17d7
Chris Tri <christophertri90@gmail.com>
2020-02-04 15:44:08 +1100

    fixing tests

--- src/test/Events.test.js
+++ src/test/Events.test.js
@@ -1,37 +1,28 @@
 import { CreateEvent } from "../pages/CreateEvent";
 import CreateEventForm from "../components/Events/EventForm/CreateEventForm";
-import WizardFormFirstPage from '../components/CreateEventForm/createformpageone'
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
 import React from "react";
 import store from "../store";
-import { shallow, mount } from "enzyme";
 
 function wrapper(component) {
   return <Provider store={store}>{component}</Provider>;
 }
 
 describe("Create Event Component", () => {
+  // console.log(render(wrapper(<CreateEvent />)).debug());
+  const { queryByText, getByText, getAllByText } = render(
+    wrapper(<CreateEvent />)
+  );
   it("tests if create event component renders", () => {
-    const { queryByText } = render(
-      wrapper(<CreateEvent />)
-    );
+    expect(queryByText("Event Name")).toBeInTheDocument();
+    expect(queryByText("Event Capacity")).toBeInTheDocument();
+  });
+  it("presses the next button and renders the next component", () => {
+    const nextPage = getAllByText("button");
+    console.log(nextPage);
 
-    queryByText("Next").click()
-    // console.log(render(
-    //   wrapper(<CreateEvent />)
-    // ).debug())
-
-    // expect(queryByText("Next").click())
-
-        console.log(render(
-      wrapper(<CreateEvent />)
-    ).debug())
-
-
-    // expect(queryByText(/Event Name/)).toBeInTheDocument();
-    // fireEvent.click(queryByText(`type="submit"`));
-
+    // fireEvent.click(queryAllByText("Next"));
   });
 
   // Tests go above this line
